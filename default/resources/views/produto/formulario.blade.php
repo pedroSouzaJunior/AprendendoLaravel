c4c738567fe9390c0e0a428ca6395a84ef8c0925
Pedro <pedro.souza.psj@gmail.com>
2018-02-03 00:04:47 -0300

    removendo bug que não permitia carregar a tela.

--- default/resources/views/produto/formulario.blade.php
+++ default/resources/views/produto/formulario.blade.php
@@ -11,14 +11,14 @@
 </div>
 @endif
 <h1>Novo produto</h1>
-{{$produto}}
+
 <form action="{{empty($produto) ? action('ProdutoController@adiciona') : action('ProdutoController@altera', $produto->id)}}" method="{{empty($produto) ? 'post' : 'put'}}">
 <input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
 
   <div class="form-group">
     <label>Nome</label>
     <input name="nome" value="{{$produto->nome or ''}}" class="form-control">
-  </div>  
+  </div>
   <div class="form-group">
     <label>Valor</label>
     <input name="valor" value="{{$produto->valor or ''}}" class="form-control">
@@ -33,7 +33,7 @@
   </div>
   <div class="form-group">
     <label>Tamanho</label>
-    <input name="tamanho" value="{{$produto->tamanho or ''}}" class="form-control" />    
+    <input name="tamanho" value="{{$produto->tamanho or ''}}" class="form-control" />
   </div>
   <button class="btn btn-primary btn-block" type="submit">{{empty($produto) ? 'Adicionar' : 'Editar'}}</button>
 </form>
